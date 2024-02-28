c584a6acb764e7ff6045e49345991550bdcb7103
Matheussmaced <mateusbyakuya3@gmail.com>
2024-02-28 11:40:26 -0300

    feat: creating layout for pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SingIn } from './pages/auth/sign-in'
 
 export const route = createBrowserRouter([
-  { path: '/', element: <Dashboard /> },
-  { path: '/sing-in', element: <SingIn /> },
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+
+  {
+    path: 'sing-in',
+    element: <AuthLayout />,
+    children: [{ path: '/sing-in', element: <SingIn /> }],
+  },
 ])
